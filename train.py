7931e616a163aaa9cc6bff84bbd97df365413b89
Sergey Kruk <sergey.kruk@gmail.com>
2023-02-12 17:38:56 +0100

    Remove debug output and minor refactor

--- train.py
+++ train.py
@@ -1,70 +1,69 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# hyperparameters
+batch_size = 32
+block_size = 8
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embed = 32
+# ---
+
+torch.manual_seed(1337)
+
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-print('Length of dataset chars: ', len(text))
-print(text[:1000])
-
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
-
-print(''.join(chars))
-print(vocab_size)
 
 stoi = { ch:i for i,ch  in enumerate(chars) }
 itos = { i:ch for i,ch  in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
 
-print(encode('hello world!'))
-print(decode(encode('hello world!')))
-
-import torch
-torch.manual_seed(1337)
-
 data = torch.tensor(encode(text), dtype=torch.long)
-print(data.shape, data.dtype)
-print(data[:1000])
-
 n = int(0.9*len(data))
 train_data = data[:n]
 val_data = data[n:]
-block_size = 8
 
-x = train_data[:block_size]
-y = train_data[1:block_size+1]
-for t in range(block_size):
-    context = x[:t+1]
-    target = y[t]
-    print(f"when context is {context}, the target is {target}")
-
-torch.manual_seed(1337)
-batch_size = 4
 def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - batch_size, (batch_size, ))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
     return x, y
 
-xb, yb = get_batch('train')
-print('inputs:')
-print(xb.shape)
-print(xb)
-print('targets:')
-print(yb.shape)
-print(yb)
-
-import torch.nn as nn
-from torch.nn import functional as F
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
-        logits = self.token_embedding_table(idx) # (B, T, C)
+        token_emb = self.token_embedding_table(idx) # (B, T, C)
+        logits = self.lm_head(token_emb) # (B, T, vocab_size)
 
         if targets is None:
             loss = None
@@ -86,17 +85,17 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-m = BigramLanguageModel(vocab_size)
-logits, loss = m(xb, yb)
-print(logits.shape)
-print(loss)
-
-print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
+model = BigramLanguageModel()
+m = model.to(device)
 
 optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
 
-batch_size = 32
-for steps in range(20000):
+for iter in range(max_iters):
+
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
     xb, yb = get_batch('train')
     logits, loss = m(xb, yb)
     optimizer.zero_grad(set_to_none=True)
@@ -104,4 +103,5 @@ for steps in range(20000):
     optimizer.step()
 
 print(loss.item())
-print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=400)[0].tolist()))
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=400)[0].tolist()))
